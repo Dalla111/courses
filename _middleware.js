5a01f406c9d420248c3c3c68dc9a9f62f9d46f42
Dalla111 <dallapushkin@gmail.com>
2025-08-19 23:14:07 +0530

    Update _middleware.js

--- _middleware.js
+++ _middleware.js
@@ -4,12 +4,12 @@ export async function onRequest(context) {
 
   // Get the Firebase config from the secure environment variables you will set in Cloudflare.
   const firebaseConfig = {
-    apiKey: context.env.VITE_FIREBASE_API_KEY,
-    authDomain: context.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: context.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: context.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: context.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: context.env.VITE_FIREBASE_APP_ID,
+    apiKey: context.env.a,
+    authDomain: context.env.d,
+    projectId: context.env.i,
+    storageBucket: context.env.b,
+    messagingSenderId: context.env.id,
+    appId: context.env.app,
   };
 
   // This creates a new script tag containing your secure config.
@@ -23,4 +23,5 @@ export async function onRequest(context) {
       },
     })
     .transform(response);
+
 }
